9bbf6fb559a5f358cb375c55219a3ffbaee35b2b
ricardonetmind <ricardoag@expert.netmind.es>
2023-04-09 23:55:33 +0200

    update 04 spring data flow

---
+++ 04.springclouddataflow/stream/demo-sink/src/test/java/es/netmind/demosink/DemoSinkApplicationTests.java
@@ -0,0 +1,13 @@
+package es.netmind.demosink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
