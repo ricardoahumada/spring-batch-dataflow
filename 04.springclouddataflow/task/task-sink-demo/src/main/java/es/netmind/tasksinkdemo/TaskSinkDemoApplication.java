ec0c635b6f1d42bef07047174a7f54575edc40a4
ricardonetmind <ricardoag@expert.netmind.es>
2023-04-10 23:44:44 +0200

    update 04 spring data flow

--- 04.springclouddataflow/task/task-sink-demo/src/main/java/es/netmind/tasksinkdemo/TaskSinkDemoApplication.java
+++ 04.springclouddataflow/task/task-sink-demo/src/main/java/es/netmind/tasksinkdemo/TaskSinkDemoApplication.java
@@ -2,9 +2,12 @@ package es.netmind.tasksinkdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.cloud.task.launcher.annotation.EnableTaskLauncher;
 
 @SpringBootApplication
+@EnableBinding(Sink.class)
 @EnableTaskLauncher
 public class TaskSinkDemoApplication {
 
