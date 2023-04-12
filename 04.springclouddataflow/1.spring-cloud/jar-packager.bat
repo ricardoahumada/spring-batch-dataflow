c5c731f535ffca86c349f5f18c80571981f5f4cf
ricardonetmind <ricardoag@expert.netmind.es>
2023-04-12 16:29:32 +0200

    update 04 spring data flow

---
+++ 04.springclouddataflow/1.spring-cloud/jar-packager.bat
@@ -0,0 +1,31 @@
+@ECHO OFF
+
+echo "Packaging config-server..."
+cd ./config-server
+mvn clean package
+cd ..
+
+echo "Packaging naming-server..."
+cd ./naming-server
+mvn clean package
+cd ..
+
+echo "Packaging gateway-server..."
+cd ./gateway-server
+mvn clean package
+cd ..
+
+echo "Packaging products-service..."
+cd ./products-service
+mvn clean package
+cd ..
+
+echo "Packaging orders-service..."
+cd ./orders-service
+mvn clean package
+cd ..
+
+echo "Packaging oauth2..."
+cd ./oauth2
+mvn clean package
+cd ..
