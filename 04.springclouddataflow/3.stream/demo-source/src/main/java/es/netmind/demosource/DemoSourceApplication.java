28489dba6009401b5e10d89f0cbb0d96cebb74e4
ricardonetmind <ricardoag@expert.netmind.es>
2023-04-17 00:02:03 +0200

    updates

--- 04.springclouddataflow/3.stream/demo-source/src/main/java/es/netmind/demosource/DemoSourceApplication.java
+++ 04.springclouddataflow/3.stream/demo-source/src/main/java/es/netmind/demosource/DemoSourceApplication.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.function.context.PollableBean;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.cloud.stream.messaging.Source;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 import java.util.UUID;
 import java.util.function.Supplier;
@@ -32,29 +34,39 @@ public class DemoSourceApplication {
         SpringApplication.run(DemoSourceApplication.class, args);
     }
 
-//    @Autowired
+    // @Autowired
     Source source; // old way
 
     @Autowired
-	private StreamBridge streamBridge;
+    private StreamBridge streamBridge;
 
     @GetMapping("")
-    void send(@RequestParam String text) {
-        log.info("Recived text: " + text);
-        Message aMessage = new Message("A title", text);
-//        source.output().send(MessageBuilder.withPayload(aMessage).build());// old way
+    void send(@RequestParam String title, @RequestParam String body) {
+        log.info("Recived: " + title + "::" + body);
+        Message aMessage = new Message(title, body);
+        // source.output().send(MessageBuilder.withPayload(aMessage).build());// old way
         streamBridge.send(Source.OUTPUT, aMessage);
     }
 
-   /* @Bean // functional approachv
+    /** functional approaches **/
+   /* @Bean
     @InboundChannelAdapter(channel = Source.OUTPUT, poller = @Poller(fixedDelay = "1000", maxMessagesPerPoll = "1"))
     MessageSource<String> source() {
         return (() -> MessageBuilder.withPayload("Hi").build());
     }*/
 
     /*@Bean
-    public Supplier<String> stringSupplier(){
+    public Supplier<String> stringSupplier() {
         return () -> "Hello from Id: " + UUID.randomUUID();
+    }*/
+
+    /**
+     * functional reactive approach
+     **/
+    /*@Bean
+    // @PollableBean
+    public Supplier<Flux<String>> stringReactSupplier() {
+        return () -> Flux.just("foo", "bar", "baz");
     }*/
 
 }
--- 04.springclouddataflow/3.stream/demo-source/src/test/java/es/netmind/demosource/DemoSourceApplicationTest.java
+++ 04.springclouddataflow/3.stream/demo-source/src/test/java/es/netmind/demosource/DemoSourceApplicationTest.java
@@ -1,6 +1,7 @@
 package es.netmind.demosource;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import es.netmind.demosource.model.Message;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +12,13 @@ import org.springframework.cloud.stream.binder.test.OutputDestination;
 import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.messaging.Message;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import java.io.IOException;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -33,12 +35,10 @@ import static org.junit.jupiter.api.Assertions.fail;
 class DemoSourceApplicationTest {
     @Autowired
     private MockMvc mvc;
-
     @Autowired
-    private InputDestination input;
-
+    private InputDestination inputDestination;
     @Autowired
-    private OutputDestination output;
+    private OutputDestination outputDestination;
 
     @Test
     void when_send_get_then_isOk() {
@@ -47,7 +47,8 @@ class DemoSourceApplicationTest {
             // when:
             //then:
             MvcResult result = mvc.perform(get("/")
-                            .param("text","Hello new!")
+                            .param("title", "This is a title")
+                            .param("body", "This is a body")
                             .accept(MediaType.TEXT_PLAIN)
                     )
                     .andDo(MockMvcResultHandlers.print())
@@ -55,6 +56,19 @@ class DemoSourceApplicationTest {
                     .andReturn();
 
             String contentAsString = result.getResponse().getContentAsString();
+
+            org.springframework.messaging.Message<byte[]> outputMessage = outputDestination.receive(1000, "demo-strm");
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            Message recMessage = null;
+            try {
+                recMessage = objectMapper.readValue(outputMessage.getPayload(), Message.class);
+            } catch (IOException e) {
+                fail(e.getMessage());
+            }
+
+            log.info("Sent message object in test {}", recMessage);
+            assertThat(outputMessage).isNotNull();
 
         } catch (Exception e) {
             fail(e.getMessage());
