35a32e6691cacc0309fbd1d6ebe91d77a0e04f28
ricardonetmind <ricardoag@expert.netmind.es>
2023-10-27 11:28:04 +0200

    update to banana

---
+++ 02.springboot/BananaInvoicesService/src/main/java/com/banana/banana_invoices/persistence/UserRepository.java
@@ -0,0 +1,12 @@
+package com.banana.banana_invoices.persistence;
+
+import com.banana.banana_invoices.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
